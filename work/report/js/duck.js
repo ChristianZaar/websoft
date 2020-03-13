3cf22bfb18c0bf82c9358f3a78f6f45d4653f4bc
ChristianZaar <christian.zaar.82@gmail.com>
2020-03-13 01:24:59 +0100

    Added missunderstod stuff

--- work/report/js/duck.js
+++ work/report/js/duck.js
@@ -1,24 +1,40 @@
 
 (function () {
+    "use strict";
+    var duck = document.createElement("img"); 
+    duck.src = "img/duck.png";
+    duck.style.width = "100px";
+    duck.style.position = "absolute";
+    document.body.appendChild(duck);
+    duck.onclick = ()=>{
+        duck.style.top = Math.floor(Math.random() * 50 + 25) + '%';
+        duck.style.left = Math.floor(Math.random() * 50 + 25) + '%'
+    };
+    var move = 0;
     setInterval(function () { 
-      blink();
-    }, 1000);
+        move++;
+        move %= 8; 
+        blink(duck, move);
+    }, 400);
   })();
 
-function blink() {
+function blink(duck, move) {
+    "use strict";
+    /*if (move === 1){
+        duck.style.top = Math.floor(Math.random() * 50 + 25) + '%';
+        duck.style.left = Math.floor(Math.random() * 50 + 25) + '%';
+    }*/
 
-    var duck  = document.getElementById("duck-img") 
-    if (document.getElementById("duck-img") != null) {
-        if (document.getElementById("duck-img").style.visibility === "visible"){
-            setTimeout(function () {
-                document.getElementById("duck-img").style.visibility = "hidden";
-            }, 500);
-        }
-        else{
-            setTimeout(function () {
-                document.getElementById("duck-img").style.visibility = "visible";
-            }, 500);
-        }
+    if (duck.style.visibility === "visible"){
+        setTimeout(function () {
+            duck.style.visibility = "hidden";
+        }, 500);
     }
+    else{
+        setTimeout(function () {
+            duck.style.visibility = "visible";
+        }, 500);
+    }
+    
 }
   
